8fcfc9bea80048b44e0aa7800e33bf5b763196a2
prakashtimilsina <80721127+prakashtimilsina@users.noreply.github.com>
2024-09-14 14:38:31 -0500

    Restapi simple test

---
+++ restapi/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import get_users, create_user, user_details
+
+
+urlpatterns = [
+    path('users/', get_users, name='get_users'),
+    path('users/create/', create_user, name='create_user'),
+    path('users/<int:id>/', user_details, name='user_details'),
+]
+
